c4869cb3993ec428745d069d3b252d0987ecb5b9
Zulwiyoza Putra <zulwiyozaputra@yahoo.com>
2017-01-23 15:18:52 +0700

    Firebase database added

---
+++ devnet/Comment.swift
@@ -0,0 +1,14 @@
+//
+//  Comment.swift
+//  devnet
+//
+//  Created by Zulwiyoza Putra on 1/23/17.
+//  Copyright © 2017 zulwiyozaputra. All rights reserved.
+//
+
+import Foundation
+
+struct Comment {
+    static var commentContent: String?
+    static var commentDate: NSDate?
+}
---
+++ devnet/Response.swift
@@ -0,0 +1,14 @@
+//
+//  Response.swift
+//  devnet
+//
+//  Created by Zulwiyoza Putra on 1/23/17.
+//  Copyright © 2017 zulwiyozaputra. All rights reserved.
+//
+
+import Foundation
+
+struct Response {
+    static var like: Int = 0
+    static var comment: Comment?
+}
