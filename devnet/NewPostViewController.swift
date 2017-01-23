83bc88a1da31fe33e6ab10c031ebf207785300bb
Zulwiyoza Putra <zulwiyozaputra@yahoo.com>
2017-01-23 22:33:40 +0700

    New Post View Initialized

---
+++ devnet/NewPostViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NewPostViewController.swift
+//  devnet
+//
+//  Created by Zulwiyoza Putra on 1/23/17.
+//  Copyright © 2017 zulwiyozaputra. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class NewPostViewController: UIViewController {
+    
+    
+    @IBAction func cancelButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func saveButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
