c0bfea204ec0566778f1e269cc418954e3cb1e42
Zulwiyoza Putra <zulwiyozaputra@yahoo.com>
2017-01-23 21:29:58 +0700

    Post object initialized

---
+++ devnet/Post.swift
@@ -0,0 +1,16 @@
+//
+//  Post.swift
+//  devnet
+//
+//  Created by Zulwiyoza Putra on 1/23/17.
+//  Copyright © 2017 zulwiyozaputra. All rights reserved.
+//
+
+import Foundation
+
+struct Post {
+    static var postTitle: String?
+    static var postModificationDate: NSDate?
+    static var postContent: String?
+    static var postResponse: Response?
+}
