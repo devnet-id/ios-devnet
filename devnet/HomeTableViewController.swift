6ab328b199cd88da9812043ae6485efbf26bbf40
Zulwiyoza Putra <zulwiyozaputra@yahoo.com>
2017-01-29 19:24:28 +0700

    Update refactoring table view

--- devnet/HomeTableViewController.swift
+++ devnet/HomeTableViewController.swift
@@ -33,11 +33,6 @@ class HomeTableViewController: UITableViewController {
             self.tableView.reloadData()
         }
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
     
 
