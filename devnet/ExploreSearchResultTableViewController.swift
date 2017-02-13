2a50c1cbb143364b20b391ee054490ca009d13e2
Zulwiyoza Putra <zulwiyozaputra@gmail.com>
2017-02-13 12:41:27 +0700

    Update TableView

--- devnet/ExploreSearchResultTableViewController.swift
+++ devnet/ExploreSearchResultTableViewController.swift
@@ -12,13 +12,7 @@ class ExploreSearchResultTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        self.clearsSelectionOnViewWillAppear = false
     }
 
     override func didReceiveMemoryWarning() {
