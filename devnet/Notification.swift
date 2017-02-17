7106e5c80592c56904fd262ef283bafb0542bcc5
Zulwiyoza Putra <zulwiyozaputra@gmail.com>
2017-02-17 23:15:11 +0700

    Notification Model initialized

---
+++ devnet/Notification.swift
@@ -0,0 +1,16 @@
+//
+//  Notification.swift
+//  devnet
+//
+//  Created by Zulwiyoza Putra on 2/17/17.
+//  Copyright © 2017 zulwiyozaputra. All rights reserved.
+//
+
+import Foundation
+
+struct Notification {
+    static var activityTitle: String?
+    static var activityModificationDate: NSDate?
+    static var activiyContent: String?
+    static var activityResponse: Response?
+}
