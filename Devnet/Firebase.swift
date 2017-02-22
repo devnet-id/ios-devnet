654adfde34e31090c88dfdfc55e653328b492957
Zulwiyoza Putra <zulwiyozaputra@gmail.com>
2017-02-23 02:36:29 +0700

    Update

--- Devnet/Firebase.swift
+++ Devnet/Firebase.swift
@@ -8,3 +8,19 @@
 
 import Foundation
 import Firebase
+
+class Firebase: NSObject {
+    
+    var ref = FIRDatabase.database().reference(fromURL: "https://devnet-12ce3.firebaseio.com/")
+    
+    
+    var uid = FIRAuth.auth()?.currentUser?.uid
+    
+    class func sharedInstance() -> Firebase {
+        struct Singleton {
+            static var shared = Firebase()
+        }
+        return Singleton.shared
+    }
+    
+}
