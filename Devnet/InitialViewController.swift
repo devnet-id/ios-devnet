c654f572292811da4c9fda770d55a84862793a37
Zulwiyoza Putra <zulwiyozaputra@gmail.com>
2017-02-23 03:14:40 +0700

    Initial View controller initialized

---
+++ Devnet/InitialViewController.swift
@@ -0,0 +1,45 @@
+//
+//  InitialViewController.swift
+//  Devnet
+//
+//  Created by Zulwiyoza Putra on 2/23/17.
+//  Copyright © 2017 Kibar. All rights reserved.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    func isUserSignedIn() {
+        if let uid = Firebase.shared().uid {
+            print("User is signed in with uid: ", uid)
+        } else {
+            print("User is not signed in")
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
