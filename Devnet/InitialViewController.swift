daa25ba799b980d885e2e853383c3e567ffdc80d
Zulwiyoza Putra <zulwiyozaputra@gmail.com>
2017-02-23 02:17:57 +0700

    Initial View Controller Added

--- Devnet/InitialViewController.swift
+++ Devnet/InitialViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class LaunchScreenViewController: UIViewController {
+class InitialViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
