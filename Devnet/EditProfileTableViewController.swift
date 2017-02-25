66079f8f8c1ec50aba4d6ffd2532c65e6a4fa0c6
Zulwiyoza Putra <zulwiyozaputra@gmail.com>
2017-02-25 20:28:59 +0700

    Photo added

--- Devnet/EditProfileTableViewController.swift
+++ Devnet/EditProfileTableViewController.swift
@@ -8,8 +8,94 @@
 
 import UIKit
 
-class EditProfileTableViewController: UITableViewController {
+class EditProfileTableViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var profilePictureButton: UIButton!
+    
+    @IBAction func profilePictureAction(_ sender: Any) {
+        handleImportPicture()
+    }
+    
+    func handleImportPicture() {
+        //Create instance of UIAlertController with title and message
+        let importController = UIAlertController()
+        
+        //Setup import from Photo Library Action
+        let importFromPhotoLibraryAction = UIAlertAction(title: "Import from Photo Library", style: UIAlertActionStyle.default) {
+            action in
+            importController.dismiss(animated: true, completion: nil)
+            self.pickAnImageFromMediaLibrary()
+        }
+        
+        //Setup import from camera action
+        let importFromCameraAction = UIAlertAction(title: "Take a Picture", style: UIAlertActionStyle.default) {
+            action in
+            importController.dismiss(animated: true, completion: nil)
+            self.pickAnImageFromCamera()
+        }
+        
+        importFromCameraAction.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        
+        //Setup cancel button
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel) {
+            action in
+            importController.dismiss(animated: true, completion: nil)
+        }
+        
+        //Tells importButton to add actions
+        importController.addAction(importFromPhotoLibraryAction)
+        importController.addAction(importFromCameraAction)
+        importController.addAction(cancelAction)
+        
+        //Tells importButton to present importController when it is tapped
+        self.present(importController, animated: true, completion: nil)
+
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
+            setImageToImageView(image: image)
+            print("image is edited")
+        } else if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            setImageToImageView(image: image)
+            print("image is original")
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func setImageToImageView(image: UIImage) -> Void {
+        profilePictureButton.setBackgroundImage(image, for: .normal)
+    }
+    
+    //Tells the delegate that the user cancelled the pick operation.
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //Setup UIImagePickerController
+    func setupImagePickerController(sourceType: UIImagePickerControllerSourceType) -> Void {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.allowsEditing = true
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = sourceType
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    //HELPERS//
+    
+    //Picking an image action from Media Library
+    func pickAnImageFromMediaLibrary() {
+        setupImagePickerController(sourceType: .photoLibrary)
+    }
+    
+    //Picking an image action from Camera
+    func pickAnImageFromCamera() {
+        setupImagePickerController(sourceType: .camera)
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
